8e09d771cec5bf779242a7d337f53fe7efcd17b9
Jonathan Aparecido de Souza Oliveira <jonathanoliver.developer@gmail.com>
2021-12-23 15:40:47 +0000

    add files

---
+++ guessing_game/guessing_game.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    cout << "********************************" << endl;
+    cout << "* Welcome to the guessing game *" << endl;
+    cout << "********************************" << endl;
+
+    //secret number
+
+    int number = 50;
+    cout << "Number secret is " <<number<<", dont tell anyone";
+}
