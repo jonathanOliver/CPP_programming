ddd8cc92b9c11a429ff9ea448abd407a445f6537
Jonathan Aparecido de Souza Oliveira <jonathanoliver.developer@gmail.com>
2021-12-28 15:43:24 +0000

    secret word implements

--- focuses/focuses.cpp
+++ focuses/focuses.cpp
@@ -1,13 +1,43 @@
 
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
+
+//global string
+const string SECRET_WORD = "MELANCIA";
+
+bool kick_exists (char kick){
+
+    int i = 0;
+    for(i; i < SECRET_WORD.size(); i++){
+        if(kick == SECRET_WORD[i] ){
+            return true;
+        }
+    }
+    
+    return false;
+}
 
 
 int main(){
 
-    string secret_char = "MELANCIA";
+    //não acertou
+    bool didnt_hitt = true;
 
-    cout << secret_char << endl;
+    //não enforcou
+    bool didnt_hang = true;
+
+    while ( didnt_hitt && didnt_hang ){
+        char kick;
+        cin >> kick;
+
+        if(kick_exists( kick )){
+             cout << "congrats Your kick correct >> " << kick << endl;
+        }else{
+            cout << "Your kick incorrect (*~*) >> " << kick << endl;
+        }
+
+    }
 
 }
