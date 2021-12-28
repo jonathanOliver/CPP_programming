383e392bf5d846834f230425e7278d0eeb5eb68c
Jonathan Aparecido de Souza Oliveira <jonathanoliver.developer@gmail.com>
2021-12-28 17:07:27 +0000

    letras

--- focuses/focuses.cpp
+++ focuses/focuses.cpp
@@ -5,11 +5,15 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <map>
 using namespace std;
 
 //global string
 const string SECRET_WORD = "MELANCIA";
 
+map <char, bool> kicking;
+
+//validate kick exist in SECRET_WORD
 bool kick_exists (char kick){
     
     for(char word : SECRET_WORD){
@@ -22,11 +26,15 @@ bool kick_exists (char kick){
 }
 
 
+
+
 int main(){
 
     cout << "************************************************************************************************" << endl;
     cout << "********************************* Welcome to the Focuses game *********************************" << endl;
     cout << "************************************************************************************************" << endl;
+
+    cout << "\n\n--- Type a letter ---" << endl;
 
     //não acertou
     bool didnt_hitt = true;
@@ -35,8 +43,19 @@ int main(){
     bool didnt_hang = true;
 
     while ( didnt_hitt && didnt_hang ){
+        for (char word : SECRET_WORD ){
+            if(kicking[word]){
+                cout << "\n\n";
+                cout << "Letter "<< word << " has already been typed >> " << endl;
+            }else{
+                cout << "_ ";
+            }
+        }
+
         char kick;
         cin >> kick;
+
+        kicking[kick] = true;
 
         if(kick_exists( kick )){
              cout << "congrats Your kick correct >> " << kick << endl;
